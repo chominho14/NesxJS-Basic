df56f5c7c32a302a8e6318eb3140a65c932e459d
chominho14 <chominho14@naver.com>
2022-02-13 14:19:45 +0900

    NextJS - 5 - Head and project pettenrs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import NavBar from "../components/NavBar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">Hello </h1>
-      <style jsx global>{``}</style>
     </div>
   );
 }

2896d2b21e878a886f63f522574954c93df7922e
chominho14 <chominho14@naver.com>
2022-02-14 17:42:28 +0900

    NextJS - 6 - API Data Fetching Petterns

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
 import Head from "next/head";
 import Seo from "../components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "6e6a3de56d7e4e0ff784cf0d24daa0dd";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello </h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
